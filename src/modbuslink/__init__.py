aafc3b3a893ed9e3f0cda6ef45b59811203b284e
MIRAITOWA\Qing <2056978412@qq.com>
2025-08-05 00:03:14 +0800

    0.1.7 增加ModbusAscii同步异步和ModbusRTU异步适配 | Add ModbusAscii synchronous and asynchronous and ModbusRTU asynchronous adaptation.

--- src/modbuslink/__init__.py
+++ src/modbuslink/__init__.py
@@ -4,7 +4,7 @@ ModbusLink - 现代化、功能强大、开发者友好且高度可扩展的Pyth
 Modern, powerful, developer-friendly and highly scalable Python Modbus library
 """
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __author__ = "Miraitowa"
 __email__ = "2056978412@qq.com"
 
@@ -14,6 +14,9 @@ from .client.async_client import AsyncModbusClient
 from .transport.rtu import RtuTransport
 from .transport.tcp import TcpTransport
 from .transport.async_tcp import AsyncTcpTransport
+from .transport.async_rtu import AsyncRtuTransport
+from .transport.ascii import AsciiTransport
+from .transport.async_ascii import AsyncAsciiTransport
 from .common.exceptions import (
     ModbusLinkError,
     ConnectionError,
@@ -28,7 +31,10 @@ __all__ = [
     "AsyncModbusClient",
     "RtuTransport",
     "TcpTransport",
+    "AsciiTransport",
+    "AsyncRtuTransport",
     "AsyncTcpTransport",
+    "AsyncAsciiTransport",
     "ModbusLinkError",
     "ConnectionError",
     "TimeoutError",
